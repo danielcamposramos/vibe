f5e0212020de4bc9f4490aa284761a8362bc3aa4
Daniel Ramos <46768340+danielcamposramos@users.noreply.github.com>
2025-07-02 01:52:54 -0300

    Add language selector to landing page

--- landing/src/components/Nav.svelte
+++ landing/src/components/Nav.svelte
@@ -3,8 +3,10 @@
 	import Discord from '~/icons/Discord.svelte'
 	import Github from '~/icons/Github.svelte'
 	import Logo from '~/icons/Logo.svelte'
-	import { i18n } from '~/lib/i18n'
-	const t = $i18n.t
+import { i18n, supportedLanguages } from '~/lib/i18n'
+const t = $i18n.t
+
+const languages = Object.entries(supportedLanguages)
 </script>
 
 <div class="navbar bg-base-100 lg:flex lg:max-w-[1065px] mt-3 m-auto">
@@ -15,9 +17,21 @@
 		</a>
 	</div>
 	<!-- links -->
-	<ul class="flex-none menu menu-horizontal px-1 gap-4" dir="ltr">
-		<a href={`https://github.com/thewh1teagle/vibe`} target="_blank">
-			<Github width="28" height="28" />
-		</a>
-	</ul>
+       <ul class="flex-none menu menu-horizontal px-1 gap-4" dir="ltr">
+               <li class="flex items-center">
+                       <label class="sr-only">{t('language')}</label>
+                       <select
+                               class="select select-bordered select-sm"
+                               on:change={(e) => i18n.changeLanguage(e.target.value)}
+                               bind:value={$i18n.language}>
+                               <option value="">{t('select-language')}</option>
+                               {#each languages as [code, name]}
+                                       <option value={code}>{name}</option>
+                               {/each}
+                       </select>
+               </li>
+               <a href={`https://github.com/thewh1teagle/vibe`} target="_blank">
+                       <Github width="28" height="28" />
+               </a>
+       </ul>
 </div>
